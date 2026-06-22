eecf1b76113a8f7631d7c3e2e66fdce947c68b03
Yuvashree R <yuvashree.ra05@gmail.com>
2026-06-22 19:43:15 +0530

    Add SimilarityAnalyzer class for similarity analysis

--- similarity.py
+++ similarity.py
@@ -3,7 +3,7 @@ from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
-#code
+
 class SimilarityAnalyzer:
     def __init__(self, model_name: str = "all-MiniLM-L6-v2"):
         self.model = SentenceTransformer(model_name)
